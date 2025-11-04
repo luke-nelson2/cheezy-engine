3deef9f54955ab8fa69dc17893ea11142b00458f
luke-nelson2 <60626461+luke-nelson2@users.noreply.github.com>
2025-11-04 15:30:08 -0600

    Create engine.cpp
    adding bitboards

---
+++ engine.cpp
@@ -0,0 +1,22 @@
+#include <cstdint>
+#include <iostream>
+
+class bitboard{
+  uint64_t white_pawn;
+  uint64_t white_knight;
+  uint64_t white_bishop;
+  uint64_t white_rook;
+  uint64_t white_queen;
+  uint64_t white_king;
+
+  uint64_t black_pawn;
+  uint64_t black_knight;
+  uint64_t black_bishop;
+  uint64_t black_rook;
+  uint64_t black_queen;
+  uint64_t black_king;
+};
+
+int main(){
+  return 0;
+}

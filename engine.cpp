f6d82eeb52526942e2c655568bd43b3947a9ce54
luke-nelson2 <60626461+luke-nelson2@users.noreply.github.com>
2025-11-07 19:33:06 -0600

    add move class

--- engine.cpp
+++ engine.cpp
@@ -1,7 +1,7 @@
 #include <cstdint>
 #include <iostream>
 
-class bitboard{
+class Bitboard{
   uint64_t white_pawn;
   uint64_t white_knight;
   uint64_t white_bishop;
@@ -16,6 +16,19 @@ class bitboard{
   uint64_t black_queen;
   uint64_t black_king;
 };
+
+class Move{
+public:
+  uint16_t move_value;
+private:
+  uint64_t Knight_Attacks[64];
+
+};
+
+// Init bitboards
+// Generate all possible moves
+// How to store moves?
+// minimax search with alpha beta pruning
 
 int main(){
   return 0;

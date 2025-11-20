39dc25f7094815024e68eb32aabae963ab697ab4
luke-nelson2 <60626461+luke-nelson2@users.noreply.github.com>
2025-11-20 16:24:40 -0600

    added knight attack table

--- engine.cpp
+++ engine.cpp
@@ -20,46 +20,111 @@ public:
 
   unsigned char c_rights;
   uint64_t ep_target;
-public:
+
   Position() {
     white_pawn = 0xFF00ULL;
     white_knight = 0x42ULL;
     white_bishop = 0x24ULL;
     white_rook = 0x81ULL;
-    white_queen = 0x10ULL;
-    white_king = 0x8ULL;
+    white_king = 0x10ULL;
+    white_queen = 0x8ULL;
 
     black_pawn = 0xFF'00'00'00'00'00'00ULL;
     black_knight = 0x42'00'00'00'00'00'00'00ULL;
     black_bishop = 0x24'00'00'00'00'00'00'00ULL;
     black_rook = 0x81'00'00'00'00'00'00'00ULL;
-    black_queen = 0x10'00'00'00'00'00'00'00ULL;
-    black_king = 0x8'00'00'00'00'00'00'00ULL;
+    black_king = 0x10'00'00'00'00'00'00'00ULL;
+    black_queen = 0x8'00'00'00'00'00'00'00ULL;
   }
-  void print_bitboard(uint64_t bb) {
-    for (int i = 63; i >= 0; i--) {
-        if (bb & (1ULL >> i)) {
+  
+};
+
+void print_bitboard(uint64_t bb) {
+    for (int i = 7; i >= 0; i--) {
+      for (int j = 0; j<=7; j++) {
+        if (bb & (1ULL << (i*8+j))) {
             std::cout << "1 "; // Print '1' if bit is set
         } else {
             std::cout << ". "; // Print '.' if bit is clear
         }
-
-        // Print a newline after every 8 bits (after h-file of a rank)
-        if (i % 8 == 0) {
-            std::cout << "\n";
-        }
+      }
+      std::cout << std::endl;
     }
     std::cout << "\n";
-  }
-};
+}
 
-class Move{
+class Attack{
 public:
   uint16_t move_value;
-private:
-  uint64_t Knight_Attacks[64];
+  static uint64_t knight_attacks[64];
+
+  static void init_knight_table(){
+    int idx;
+    
+    for (int rank=0; rank<8; rank++){
+      for (int file=0; file<8; file++){
+        uint64_t mask{0x0};
+        idx = file*8+rank;
+        uint64_t piece_bit = 1ULL << idx;
+        uint64_t target;
+
+        // SSW
+        if (rank>0 && file>1) {
+          target = piece_bit >> 17;
+          mask |= target;
+        }
+
+        // SSE
+        if (rank<7 && file>1) {
+          target = piece_bit >> 15;
+          mask |= target;
+        }
+
+        // ESE
+        if (rank<6 && file>0) {
+          target = piece_bit >> 6;
+          mask |= target;
+        }
+
+        // ENE
+        if (rank<6 && file<7) {
+          target = piece_bit << 10;
+          mask |= target;
+        }
+
+        // NNE
+        if (rank<7 && file<6) {
+          target = piece_bit << 17;
+          mask |= target;
+        }
+
+        // NNW
+        if (rank>0 && file<6) {
+          target = piece_bit << 15;
+          mask |= target;
+        }
+
+        // WNW
+        if (rank>1 && file<7) {
+          target = piece_bit << 6;
+          mask |= target;
+        }
+
+        // WSW
+        if (rank>1 && file>0) {
+          target = piece_bit >> 10;
+          mask |= target;
+        }
+
+
+        knight_attacks[idx] = mask;
+      }
+    }
+  }
 
 };
+
+uint64_t Attack::knight_attacks[64];
 
 // TODO:
 // Init bitboards
@@ -78,8 +143,12 @@ private:
 // Artificially deflate the elo of the engine
 // Setup to be fully compliant with UCI and able to play other engines
 
+
+
 int main(){
-  Position board{};
-  board.print_bitboard(board.white_king);
+  //Position board{};
+  //print_bitboard(board.black_pawn);
+  Attack::init_knight_table();
+  print_bitboard(Attack::knight_attacks[63]);
   return 0;
 }
